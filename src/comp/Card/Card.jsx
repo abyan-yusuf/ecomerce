6a5a7ff3be49080a88c2ea4da18e22c99ec6ce73
Abyan Yusuf <abyan.yousuf@gmail.com>
2023-10-17 18:05:43 +0600

    Updated

--- src/comp/Card/Card.jsx
+++ src/comp/Card/Card.jsx
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 const Card = ({ products }) => {
   return (
-    <div className="card w-80 shadow-xl bg-slate-800">
-      <figure className="bg-white">
-        <img
-          src={products.image}
-          className="h-56 w-auto"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {'Tk. ' + products.title}
-        </h2>
-        <p>{products.price}</p>
-        <div className="card-actions justify-end">
-          <Link to={`/products/${products.id}`} className="btn btn-primary">Details</Link>
+    <Link to={`/products/${products.id}`}>
+      <div className="card w-80 shadow-xl bg-slate-800">
+        <figure className="bg-white">
+          <img src={products.image} className="h-56 w-auto" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{"Tk. " + products.title}</h2>
+          <p>{products.price}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
-}
+};
 
-export default Card
+export default Card;
